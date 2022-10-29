3f5ef97f192c6dbe6cb06bb3249993943eae3f7c
Adithya S K <adithyaskolavi@gmail.com>
2022-10-29 11:47:05 +0530

    void loop for serial monitor added - Adithya S K

--- ESP Projects/Temperature Humidity measure using ESP32/DHT22_Serial_monitor/DHT22_Serial_monitor.ino
+++ ESP Projects/Temperature Humidity measure using ESP32/DHT22_Serial_monitor/DHT22_Serial_monitor.ino
@@ -31,3 +31,38 @@ void setup() {
 
   dht.begin();
 }
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
