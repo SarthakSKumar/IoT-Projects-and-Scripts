1f336440c28ec9d11ffca2001c20b70b3f58139d
Adithya S K <adithyaskolavi@gmail.com>
2022-10-29 11:49:02 +0530

    void set up added - Adithya S  K

--- ESP Projects/Temperature Humidity measure using ESP32/DHT22_Web_server/DHT22_Web_server.ino
+++ ESP Projects/Temperature Humidity measure using ESP32/DHT22_Web_server/DHT22_Web_server.ino
@@ -124,3 +124,39 @@ String processor(const String& var){
   }
   return String();
 }
+
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  dht.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
